0d0ff51fc811187590897a44c2dd16edacf29e66
Piyush Sahal <75487373+PiyushSahal@users.noreply.github.com>
2023-12-10 12:03:02 +0530

    Add files via upload

---
+++ AddVisitorsToVisitors.ps1
@@ -0,0 +1,29 @@
+# Import the SharePoint Online module
+#Connect-SPOService -Url https://yourtenant-admin.sharepoint.com
+
+# Define the path to the CSV file
+$csvFilePath = "E:\Scripts\SPO\VisitorList.csv"
+
+# Read the CSV file
+$userList = Import-Csv -Path $csvFilePath
+
+# Loop through each row in the CSV file
+foreach ($userRow in $userList) {
+    $siteUrl = $userRow.SiteUrl
+    $userEmails = $userRow.Useremail -split ';'
+
+    # Get the default "Members" group
+    $group = Get-SPOSiteGroup -Site $siteUrl | Where-Object { $_.Roles -contains "Read" } | Select-Object -ExpandProperty Title
+
+    foreach ($userEmail in $userEmails) {
+        $userEmail = $userEmail.Trim()
+
+            # Add the user to the group
+            Add-SPOUser -Site $siteUrl -LoginName $userEmail -Group $group
+            Write-Host "User with email $userEmail added to the default 'Members' group in site $siteUrl"
+       
+    }
+}
+
+# Disconnect from SharePoint Online
+Disconnect-SPOService
